90dee4cc8d32c8b042a7db80844abeb0adec8bc9
Anastasia <greatest.say@gmail.com>
2018-10-07 18:29:20 +0300

    footer and header features

---
+++ arlen/Components/FooterPartial.cs
@@ -0,0 +1,22 @@
+﻿using arlen.Infrastructure;
+using arlen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace arlen.Components
+{
+    public class FooterPartial : ViewComponent
+    {
+        User account;
+        
+        public FooterPartial(ArlenContext db)
+        {
+            AccountManager accountManager = new AccountManager(db);
+            account = accountManager.GetAccount(1);
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(account);
+        }
+    }
+}
